47abcaf13c6e83bf97ffe3ebdb57098f032282d3
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-11-10 14:10:59 +0100

    be sure

---
+++ test/test.js
@@ -0,0 +1,23 @@
+const should = require('chai').should()
+
+const be = require('./..')
+
+describe('be-of-type', () => {
+  it('should be an object', () => {
+    be.should.be.an('object')
+  })
+  it('should have a bumnch of methods', () => {
+    Object.keys(be).map(k => be[k])
+      .every(v => v.should.be.a('function'))
+  })
+  it('should recognize types correctly', () => {
+    be.string('').should.be.true
+    be.number(0b01).should.be.true
+    be.plainObject({}).should.be.true
+    be.array(process.argv).should.be.true
+    be.boolean(0).should.be.false
+  })
+  it('should not consider objects without a prototype as objects', () => {
+    be.plainObject(Object.create(null)).should.be.false
+  })
+})
